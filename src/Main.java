f72ad8a935fc9a008cab0a5605e8870a2edfeaab
farouk773 <faroukbouraoui9@gmail.com>
2025-11-17 11:33:30 +0100

    change main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-
 public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Projet collaboratif - TP2");
-		System.out.println("Version avec nouvelle fonctionnalité !"); 
-	}
-
+public static void main(String[] args) {
+System.out.println("Projet collaboratif - TP2 (version B)");
 }
+}
+
